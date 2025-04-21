457b22460f6768579a81ff6a541d4ee9d9ab4ab1
WellisonSilvaa <wonsilva100@gmail.com>
2025-04-21 13:08:45 -0300

    feat: criado login e index

--- src/main/java/com/appLogin/appLogin/controller/LoginController.java
+++ src/main/java/com/appLogin/appLogin/controller/LoginController.java
@@ -2,10 +2,12 @@ package com.appLogin.appLogin.controller;
 
 import com.appLogin.appLogin.model.Usuario;
 import com.appLogin.appLogin.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +20,24 @@ public class LoginController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/")
+    public String dashboard() { return "index"; }
+
+
+    @PostMapping("/logar")
+    public String loginUser(Usuario user, Model model, HttpServletResponse response) {
+        Usuario userLogado = this.ur.login(user.getEmail(), user.getPassword());
+        System.out.println(userLogado);
+        if(userLogado != null) {
+            return "redirect:/";
+
+        }
+
+        model.addAttribute("erro", "Usuario Inválido");
+        return "redirect:/login";
+
     }
 
 
--- src/main/java/com/appLogin/appLogin/repository/UserRepository.java
+++ src/main/java/com/appLogin/appLogin/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.appLogin.appLogin.repository;
 
 import com.appLogin.appLogin.model.Usuario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<Usuario, String> {
 
     Usuario findById(long id);
+
+    @Query(value ="SELECT * FROM applogin.table_user WHERE email = :email AND password = :password", nativeQuery = true)
+    public Usuario login(String email, String password);
+
 }
